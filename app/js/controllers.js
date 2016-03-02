b82a20e0a9a709f89e5bec27dc4a42827a7f303e
becckitt <hello@beccabarton.com>
2016-03-01 21:13:55 -0500

    adds fun changing emoji in the header because why not

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,7 @@
 
 var emojiApp = angular.module('emojiApp', []);
 
-emojiApp.controller('EmojiListCtrl', function($scope, $http) {
+emojiApp.controller('EmojiListCtrl', function($scope, $http, $interval) {
   $http.get('https://api.github.com/emojis').success(function(data) {
     var restructuredJson = [];
     for(var emoji in data) {
@@ -10,5 +10,9 @@ emojiApp.controller('EmojiListCtrl', function($scope, $http) {
       restructuredJson.push({name: normalizedName, link: data[emoji]});
     }
     $scope.emojis = restructuredJson;
+    $scope.randomEmoji = function() {
+      return $scope.emojis[(Math.floor(Math.random() * $scope.emojis.length))];
+    }
+    $interval( function() {$scope.randomEmoji(); }, 2000);
   });
 });
