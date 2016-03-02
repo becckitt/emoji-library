37b8c3e5de65e4ee826f71cc595079d5fddfba68
becckitt <hello@beccabarton.com>
2016-03-01 20:18:16 -0500

    normalize emoji name since people probably won't be joining their words with underscores

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,7 +6,8 @@ emojiApp.controller('EmojiListCtrl', function($scope, $http) {
   $http.get('https://api.github.com/emojis').success(function(data) {
     var restructuredJson = [];
     for(var emoji in data) {
-      restructuredJson.push({name: emoji, link: data[emoji]});
+      var normalizedName = emoji.replace("_", " ");
+      restructuredJson.push({name: normalizedName, link: data[emoji]});
     }
     $scope.emojis = restructuredJson;
   });
